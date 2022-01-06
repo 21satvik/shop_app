a132e65d27a9997d5fbd3838a63f977bb53e61e1
Satvik Kumar <satvikkumar@Satviks-MacBook-Air.local>
2022-01-06 17:35:57 +0530

    route animations

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import './screens/user_products_screen.dart';
 import './screens/auth_screen.dart';
 import './screens/splash_screen.dart';
 import './providers/auth.dart';
+import './helpers/custom_route.dart';
 
 void main() => runApp(MyApp());
 
@@ -47,6 +48,10 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.purple,
             accentColor: Colors.deepOrange,
             fontFamily: 'Lato',
+            pageTransitionsTheme: PageTransitionsTheme(builders: {
+              TargetPlatform.android: CustomPageTransitionBuilder(),
+              TargetPlatform.iOS: CustomPageTransitionBuilder(),
+            }),
           ),
           home: auth.isAuth
               ? ProductsOverviewScreen()
